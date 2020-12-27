bc85f7ff0e8b3cc24def5abd99b75e2ef37e92ea
алина человечкова <alinasmart19@mail.ru>
2020-12-27 21:05:37 +0600

    Добавьте файлы проекта.

---
+++ Homework6/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Homework7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int arraySize;
+            Console.WriteLine("Введите начало диапазона: ");
+            int arrayBegin = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите конец диапазона: ");
+            int arrayEnd = int.Parse(Console.ReadLine());
+
+            if (arrayBegin < 0 && arrayEnd > 0)
+            {
+                arraySize = -arrayBegin + arrayEnd;
+                ++arraySize;
+            }else
+            {
+                arraySize = arrayEnd - arrayBegin;
+                arraySize++;
+            }
+
+
+            var someArray = new RangeOfArray(arraySize);
+            someArray.First = arrayBegin;
+            someArray.Last = arrayEnd;
+
+            for (int i= someArray.First; i <= someArray.Last; i++)
+            {
+                Console.Write("Введите значение массива: ");
+                someArray[i]= Console.ReadLine();
+            }
+
+            for (int i = someArray.First; i <= someArray.Last; i++)
+            {
+                Console.Write($"{someArray[i]}  ");
+            }
+
+           
+
+        }
+    }
+}
---
+++ Homework6/RangeOfArray.cs
@@ -0,0 +1,69 @@
+﻿
+namespace Homework7
+{
+    public class RangeOfArray
+    {
+        public int First { get; set; }
+        public int Last { get; set; }
+        private int length;
+
+
+        private string[] userName;
+
+        public RangeOfArray() { }
+        public RangeOfArray(int length)
+        {
+            this.length = length;
+            userName = new string[length];
+        }
+
+        public int GetLength()
+        {
+            return length;
+        }
+
+        public string this[int index]
+        {
+            get
+            {
+                if (index < 0 && index == First)
+                {
+                    index = index - index;
+                }
+                else if (index < 0 && index > First)
+                {
+                    index = -(First - index);
+                }
+                else if (index >= 1 && First >= 1)
+                {
+                    index = index - First;
+                }
+                else if (index >= 0 && First < 0)
+                {
+                    index = index - First;
+                }
+                return userName[index];
+            }
+            set
+            {
+                if(index < 0 && index == First)
+                {
+                    index = index - index;
+                }
+                else if (index < 0 && index > First)
+                {
+                    index = -(First - index);
+                }
+                else if (index >= 1 && First >= 1)
+                {
+                    index = index - First;
+                }
+                else if(index >=0 && First < 0)
+                {
+                    index = index - First;
+                }
+                userName[index]=value;
+            }
+        }
+    }
+}
